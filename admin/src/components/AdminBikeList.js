8d9eef856b9e4046769ae47dbbea8725d561fba3
Melissa Johansson <mejo22@student.bth.se>
2024-01-29 14:41:27 +0100

    Add off btn, fix list

--- admin/src/components/AdminBikeList.js
+++ admin/src/components/AdminBikeList.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from "react";
 import * as qs from 'qs';
 
 let chargeBtn;
+let offBtn;
+let bikeUser;
 
 const AdminBikeList = ({city, coords}) => {
     const [bikeInfo, setBikeInfo] = useState("");
     const [bikeName, setBikeName] = useState("");
     const [coordinates, setCoordinates] = useState("");
-    const [zoomLevel, setZoomLevel] = useState(5);
+    const [zoomLevel, setZoomLevel] = useState(13);
     const [bikes, setBikes] = useState("");
     const[stations, setStations] = useState("");
     const[loading, setLoading] = useState("");
@@ -25,13 +27,14 @@ const AdminBikeList = ({city, coords}) => {
         // nollställ inforutan om man byter stad och har klickat på en cykel
         setBikeInfo("");
         chargeBtn = "";
+        offBtn = "";
         // justera var kartan visar när ingen cykel är klickad
         setCoordinates(coords);
-        setZoomLevel(9);
+        setZoomLevel(13);
         setBikeName("");
     }, [city, loading])
 
-    function onClick(bike) {
+    function onClick(e, bike) {
         let bikeVal = bikes.find(b => b.id === bike);
 
         setBikeName("Cykel " + bikeVal.id);
@@ -56,19 +59,28 @@ const AdminBikeList = ({city, coords}) => {
             station = `parkering ${bikeVal.park_id}`
         }
 
+        if (bikeVal.user_id === 0 ? bikeUser = "ingen" : bikeUser = bikeVal.user_id);
+
         setBikeInfo(`Status: ${status}.
             Finns på ${station}.
-            Hyrs av kund ${bikeVal.user_id} (0 = ingen).
+            Hyrs av kund: ${bikeUser}.
             Hastighet: ${bikeVal.speed} km/h
             Batterinivå: ${bikeVal.battery} %`);
         setCoordinates([bikeVal.lat, bikeVal.lon]);
         setZoomLevel(22);
 
-        if (bikeVal.battery < 30 & bikeVal.status_id !== 3) {
+        if (bikeVal.battery < 30 & bikeVal.status_id !== 3 & bikeVal.status_id !== 1) {
             chargeBtn = <button onClick={() => onChargeClick(bikeVal)} className="chargeBtn">Begär laddning</button>
         } else {
             chargeBtn = "";
         }
+
+        offBtn = <button onClick={() => onOffClick(bikeVal)} className="offBtn">Stäng av cykel</button>;
+
+        let toggled = document.getElementsByClassName("marked")[0];
+
+        if (toggled) {toggled.className = ""};
+        e.target.className = "marked";
     }
 
     function onChargeClick(bike) {
@@ -100,21 +112,51 @@ const AdminBikeList = ({city, coords}) => {
                     headers: {
                         'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
                     },
-                    body: qs.stringify({id: bike.id, city_id: city, user_id: 0, status_id: 3, station_id: station, park_id: 0, lat: lat, lon: lon, speed: 0, battery: bike.battery})
+                    body: qs.stringify({id: bike.id, city_id: city, user_id: 0, status_id: 0, station_id: station, park_id: 0, lat: lat, lon: lon, speed: 0, battery: bike.battery})
+                })
+                .then(response => response.json())
+                .then(data => console.log(data))
+
+                fetch(`http://localhost:1337/api/v1/bikes/start_charge`, {
+                    method: 'PUT',
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                    },
+                    body: qs.stringify({bike_id: bike.id, station_id: station})
                 })
                 .then(response => response.json())
                 .then(data => console.log(data))
 
                 setLoading(bike.id);
+                alert("Cykel schemalagd för laddning");
 
                 break;
             }
-
-            alert("Cykel schemalagd för laddning");
+            // setCoordinates([bike.lat, bike.lon]);
+            // coords = [bike.lat, bike.lon];
+            // setZoomLevel(22);
         }
     }
 
-    const listItems = Array.from(bikes).map(bike => <li key={bike.id} onClick={() => onClick(bike.id)}>Cykel {bike.id}. Status: {bike.status_id}, {bike.rent} {bike.position} Batteri: {bike.battery}</li>);
+    function onOffClick(bike) {
+        console.log(bike)
+
+        fetch(`http://localhost:1337/api/v1/bikes/user_status_station_park`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            },
+            body: qs.stringify({id: bike.id, user_id: 0, status_id: 2, station_id: bike.station_id, park_id: bike.park_id})
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+
+        setLoading(bike.id);
+
+        alert("Cykel avstängd");
+    }
+
+    const listItems = Array.from(bikes).map(bike => <li key={bike.id} id={`Cykel ${bike.id}`} onClick={(e) => onClick(e, bike.id)}>Cykel {bike.id}. Status: {bike.status_id}, {bike.rent} {bike.position} Batteri: {bike.battery}</li>);
 
     return (
         <>
@@ -125,7 +167,7 @@ const AdminBikeList = ({city, coords}) => {
                 <div>
                     <h1>{bikeName}</h1>
                     <AdminMap className="admin-bikes-map" mapPosition={coordinates} zoomLevel={zoomLevel} icons="Cyklar" cityId={city} reload={loading}/>
-                    <div className="admin-bikes-info"><div className="bikeInfo">{bikeInfo}</div><div>{chargeBtn}</div></div>
+                    <div className="admin-bikes-info"><div className="bikeInfo">{bikeInfo}</div><div>{offBtn}{chargeBtn}</div></div>
                 </div>
             </div>
         </>
